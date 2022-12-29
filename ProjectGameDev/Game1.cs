7be17cb91ad6706f143d3e5ee14a063d56e31486
Redcommander <45720158+Redcommander@users.noreply.github.com>
2022-12-29 15:56:30 +0100

    Support window scaling (fullscreen etc)

--- ProjectGameDev/Game1.cs
+++ ProjectGameDev/Game1.cs
@@ -14,6 +14,8 @@ using Serilog.Core;
 
 namespace ProjectGameDev
 {
+    // Scaling code was taken from https://community.monogame.net/t/how-do-i-make-full-screen-stretch-to-the-entire-screen-and-have-black-bars-on-the-sides-if-the-screen-aspect-ratio-isnt-16-9/17364/2
+
     public class Game1 : Game
     {
         public const string GameName = "ProjectGameDev";
@@ -24,6 +26,11 @@ namespace ProjectGameDev
         private readonly DependencyManager dependencyManager;
         private readonly Core.Engine engine;
         private readonly World world;
+
+        private readonly Point gameResolution = new(32 * 30, 32 * 20);
+        private RenderTarget2D renderTarget;
+        private Rectangle renderTargetDestination;
+
 
         public Game1()
         {
@@ -42,7 +49,7 @@ namespace ProjectGameDev
             dependencyManager.RegisterDependency(animationBuilder);
 
             engine = new Core.Engine(dependencyManager);
-            Window.AllowUserResizing = true;
+            //Window.AllowUserResizing = true;
         }
 
         // We want the Crash Handler to be able to access the logger
@@ -52,6 +59,37 @@ namespace ProjectGameDev
             return dependencyManager.GetDependencyChecked<Logger>();
         }
 
+        Rectangle GetRenderTargetDestination(Point resolution, int preferredBackBufferWidth, int preferredBackBufferHeight)
+        {
+            float resolutionRatio = (float)resolution.X / resolution.Y;
+            float screenRatio;
+            Point bounds = new Point(preferredBackBufferWidth, preferredBackBufferHeight);
+            screenRatio = (float)bounds.X / bounds.Y;
+            float scale;
+            Rectangle rectangle = new Rectangle();
+
+            if (resolutionRatio < screenRatio)
+                scale = (float)bounds.Y / resolution.Y;
+            else if (resolutionRatio > screenRatio)
+                scale = (float)bounds.X / resolution.X;
+            else
+            {
+                // Resolution and window/screen share aspect ratio
+                rectangle.Size = bounds;
+                return rectangle;
+            }
+            rectangle.Width = (int)(resolution.X * scale);
+            rectangle.Height = (int)(resolution.Y * scale);
+            return CenterRectangle(new Rectangle(Point.Zero, bounds), rectangle);
+        }
+
+        static Rectangle CenterRectangle(Rectangle outerRectangle, Rectangle innerRectangle)
+        {
+            Point delta = outerRectangle.Center - innerRectangle.Center;
+            innerRectangle.Offset(delta);
+            return innerRectangle;
+        }
+
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -59,9 +97,12 @@ namespace ProjectGameDev
             //_graphics.IsFullScreen = true;
             //_graphics.ApplyChanges();
 
-            graphics.PreferredBackBufferHeight = 32 * 20;
-            graphics.PreferredBackBufferWidth = 32 * 30;
+            graphics.PreferredBackBufferHeight = gameResolution.Y;
+            graphics.PreferredBackBufferWidth = gameResolution.X;
             graphics.ApplyChanges();
+
+            renderTarget = new RenderTarget2D(GraphicsDevice, gameResolution.X, gameResolution.Y);
+            renderTargetDestination = GetRenderTargetDestination(gameResolution, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
 
             dependencyManager.RegisterDependency(GraphicsDevice);
             dependencyManager.RegisterDependency(graphics);
@@ -110,6 +151,9 @@ namespace ProjectGameDev
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            if (Keyboard.GetState().IsKeyDown(Keys.F))
+                ToggleFullScreen();
+
             // TODO: Add your update logic here
             //hero.Update(gameTime);
 
@@ -118,12 +162,41 @@ namespace ProjectGameDev
             base.Update(gameTime);
         }
 
+        void ToggleFullScreen()
+        {
+            if (!graphics.IsFullScreen)
+            {
+                graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+                graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            }
+            else
+            {
+                graphics.PreferredBackBufferWidth = gameResolution.X;
+                graphics.PreferredBackBufferHeight = gameResolution.Y;
+            }
+            graphics.IsFullScreen = !graphics.IsFullScreen;
+            graphics.ApplyChanges();
+
+            renderTargetDestination = GetRenderTargetDestination(gameResolution, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+        }
+
         protected override void Draw(GameTime gameTime)
         {
+            GraphicsDevice.SetRenderTarget(renderTarget);
             GraphicsDevice.Clear(world.BackgroundColor);
 
             spriteBatch.Begin();
             engine.Draw(spriteBatch);
+            spriteBatch.End();
+
+            GraphicsDevice.SetRenderTarget(null);
+            GraphicsDevice.Clear(world.BackgroundColor);
+
+            //renderTargetDestination = GraphicsDevice.Viewport.Bounds;
+
+            spriteBatch.Begin();
+            //engine.Draw(spriteBatch);
+            spriteBatch.Draw(renderTarget, renderTargetDestination, Color.White);
             spriteBatch.End();
 
             base.Draw(gameTime);
