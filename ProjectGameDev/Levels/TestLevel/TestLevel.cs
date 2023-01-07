b824456fccfe2f9a8007fe14379b0b6f617a4529
Redcommander <45720158+Redcommander@users.noreply.github.com>
2023-01-07 15:23:10 +0100

    Add more platforms in level 1

--- ProjectGameDev/Levels/TestLevel/TestLevel.cs
+++ ProjectGameDev/Levels/TestLevel/TestLevel.cs
@@ -20,6 +20,8 @@ namespace ProjectGameDev.Levels.TestLevel
     {
         public TestLevel(DependencyManager dependencyManager) : base(dependencyManager) { }
 
+        // @NOTE: Platforms are added manually because we don't want to align them to the grid
+
         public override void Load()
         {
             AddObject(new Background(dependencyManager, "Backgrounds/background_0"));
@@ -31,20 +33,37 @@ namespace ProjectGameDev.Levels.TestLevel
             //var dependencyManager = new Engine.DependencyManager();
             AddObject(new Hero2(dependencyManager));
             //AddObject(new Platform(dependencyManager, new Vector2(500, 450)));
-            var platform = new MovingPlatform(dependencyManager, new Vector2(500, 450), 45, 70);
+            var platform = new MovingPlatform(dependencyManager, new Vector2(500, 460), 45, 70);
             AddObject(platform);
 
+            AddObject(new Platform(dependencyManager, new Vector2(350, 480)));
+
             var slime = new GreenSlime(dependencyManager);
-            slime.GetComponentFast<RootComponent>().Move(new Vector2(530, 400));
+            slime.GetComponentFast<RootComponent>().Move(new Vector2(525, 400));
             AddObject(slime);
 
             var orangeSlime = new OrangeSlime(dependencyManager);
-            orangeSlime.GetComponentFast<RootComponent>().Move(new Vector2(700, 300));
+            orangeSlime.GetComponentFast<RootComponent>().Move(new Vector2(700, 480));
             AddObject(orangeSlime);
 
-            var undead = new Undead(dependencyManager);
-            undead.GetComponentFast<RootComponent>().Move(new Vector2(800, 300));
-            AddObject(undead);
+            // layer 2
+
+            AddObject(new Platform(dependencyManager, new Vector2(740, 410)));
+            AddObject(new Platform(dependencyManager, new Vector2(550, 370)));
+            AddObject(new MovingPlatform(dependencyManager, new Vector2(295, 350), 50, 60));
+            AddObject(new Platform(dependencyManager, new Vector2(140, 370)));
+            AddObject(new Platform(dependencyManager, new Vector2(10, 300)));
+
+            // layer 3
+
+            AddObject(new Platform(dependencyManager, new Vector2(140, 230)));
+            AddObject(new Platform(dependencyManager, new Vector2(360, 220)));
+            AddObject(new Platform(dependencyManager, new Vector2(590, 240)));
+            AddObject(new Platform(dependencyManager, new Vector2(800, 240)));
+
+            //var undead = new Undead(dependencyManager);
+            //undead.GetComponentFast<RootComponent>().Move(new Vector2(800, 300));
+            //AddObject(undead);
 
 
             //AddObject(new Hero2(dependencyManager));
